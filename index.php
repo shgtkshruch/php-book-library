45c7ac090e6c1f391d11a92c545b496f43de63b4
shgtkshruch <shgtk.shruch@gmail.com>
2015-01-09 17:08:29 +0900

    Connect MySQL database.

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+
+require_once 'login.php';
+$db_server = mysql_connect($db_hostname, $db_username, $db_password);
+
+if (!$db_server) {
+  die("Unable to connect to MySQL: " . mysql_error());
+}
+
+mysql_select_db($db_database, $db_server)
+  or die("Unable to select database: " . mysql_error());
