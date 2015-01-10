33b9a09ae7b535f040dcab54134d2066d4179b4a
shgtkshruch <shgtk.shruch@gmail.com>
2015-01-10 10:27:18 +0900

    Display book list from database to browser.

--- index.php
+++ index.php
@@ -9,3 +9,27 @@ if (!$db_server) {
 
 mysql_select_db($db_database, $db_server)
   or die("Unable to select database: " . mysql_error());
+
+$query = "SELECT * FROM classics";
+$result = mysql_query($query);
+
+if (!$result) {
+  die("Database access failed: " . mysql_error());
+}
+
+$rows = mysql_num_rows($result);
+
+for ($i = 0; $i < $rows; $i++) {
+  $row = mysql_fetch_row($result);
+
+  echo <<< EOF
+  <pre>
+    Author:   $row[0]
+    Title:    $row[1]
+    Category: $row[2]
+    Year:     $row[3]
+    ISDN:     $row[4]
+  </pre>
+EOF;
+}
+
